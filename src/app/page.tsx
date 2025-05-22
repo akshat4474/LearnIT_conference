7164678a0020d2b258b7daac5101f7975a3034c6
Akshat Bisht <akshatyogeshbisht@gmail.com>
2025-05-22 18:06:28 +0530

    Design 3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import Header from '../../UI/Design 3/Header';
 // import Hero from '../../UI/Design 1/Hero';
 // import Hero from '../../UI/Design 2/Hero';
-import Hero from '../../UI/Design 2/Hero';
+import Hero from '../../UI/Design 3/Hero';
 // import Footer from '../../UI/Design 1/Footer';
 // import Footer from '../../UI/Design 2/Footer';
 import Footer from '../../UI/Design 3/Footer';
